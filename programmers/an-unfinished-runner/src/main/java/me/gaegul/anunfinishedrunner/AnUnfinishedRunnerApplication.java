d4e8d53f53e56bacec0560efd81ed859be2aa385
gaegulgaegul <dla9721@gmail.com>
2021-04-04 23:23:19 +0900

    프로그래머스 완주하지 못한 선수

---
+++ programmers/an-unfinished-runner/src/main/java/me/gaegul/anunfinishedrunner/AnUnfinishedRunnerApplication.java
@@ -0,0 +1,13 @@
+package me.gaegul.anunfinishedrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnUnfinishedRunnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnUnfinishedRunnerApplication.class, args);
+    }
+
+}
---
+++ programmers/an-unfinished-runner/src/test/java/me/gaegul/anunfinishedrunner/AnUnfinishedRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gaegul.anunfinishedrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnUnfinishedRunnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
