0761f64b3a49485fa66fa26421a8332450f16f0e
gaegulgaegul <dla9721@gmail.com>
2021-05-02 14:32:15 +0900

    큰 수 만들기

---
+++ programmers/src/test/java/me/gaegul/programmers/큰_수_만들기/SolutionTest.java
@@ -0,0 +1,36 @@
+package me.gaegul.programmers.큰_수_만들기;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+    @Test
+    void test1() {
+        String number = "1924";
+        int k = 2;
+        Solution solution = new Solution();
+        String result = solution.solution(number, k);
+        assertEquals(result, "94");
+    }
+
+    @Test
+    void test2() {
+        String number = "1231234";
+        int k = 3;
+        Solution solution = new Solution();
+        String result = solution.solution(number, k);
+        assertEquals(result, "3234");
+    }
+
+    @Test
+    void test3() {
+        String number = "4177252841";
+        int k = 4;
+        Solution solution = new Solution();
+        String result = solution.solution(number, k);
+        assertEquals(result, "775841");
+    }
+
+}
