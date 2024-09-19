751c3b76a2a111ea10aa9f463593ad5c3020225c
gaegulgaegul <dla9721@gmail.com>
2024-09-19 13:15:12 +0900

    최소직사각형 문제 풀이 주석 추가

--- src/main/java/me/gaegle/programmers/최소직사각형/Solution.java
+++ src/main/java/me/gaegle/programmers/최소직사각형/Solution.java
@@ -1,10 +1,20 @@
 package me.gaegle.programmers.최소직사각형;
 
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/86491
+ */
 public class Solution {
+
 	public int solution(int[][] sizes) {
 		int maxWidth = 0;
 		int maxHeight = 0;
 
+		/*
+		 * 문제의 핵심은 명함을 뒤집을 수 있다는 것!
+		 *
+		 * 가로 -> 주어진 가로, 세로 중 큰 값
+		 * 세로 -> 주어진 가로, 세로 중 작은 값
+		 */
 		for (int[] size : sizes) {
 			int width = Math.max(size[0], size[1]);
 			int height = Math.min(size[0], size[1]);
